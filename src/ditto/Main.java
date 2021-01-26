9183bafded8208b049f321f538a6a5d6e18dd403
AwesomePossum50 <12309261+AwesomePossum50@users.noreply.github.com>
2021-01-26 11:53:27 -0800

    Adding Silly Line to revert.

--- src/ditto/Main.java
+++ src/ditto/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args)  {
         System.out.println("Hello, my name is Christopher");
         System.out.printf("Hello, Christopher, My name is %s.", "Brayton");
+        //Here's some stuff I'm gonna revert
+        System.out.println("This is a silly line, he eats raw hockey pucks...");
     }
 }
