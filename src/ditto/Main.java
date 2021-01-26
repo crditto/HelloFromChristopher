a9025cf0cf00ae15b1b0d486b7e7bca79e73aa41
AwesomePossum50 <12309261+AwesomePossum50@users.noreply.github.com>
2021-01-26 11:49:10 -0800

    Saying hello.

--- src/ditto/Main.java
+++ src/ditto/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args)  {
         System.out.println("Hello, my name is Christopher");
+        System.out.printf("Hello, Christopher, My name is %s.", "Brayton");
     }
 }
