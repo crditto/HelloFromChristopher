6ef105c65f2506ae835d40b541d0baf6794c0abd
canuck <darrenw@byui.edu>
2021-01-26 22:02:53 -0700

    Darren's Changes

--- src/ditto/Main.java
+++ src/ditto/Main.java
@@ -1,9 +1,21 @@
 package ditto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args)  {
-        System.out.println("Hello, my name is Christopher");
-        System.out.printf("Hello, Christopher, My name is %s.", "Brayton");
+    public static void main(String[] args) {
+        String yourName = "Christopher";
+        List<String> teamMatesNames = new ArrayList<>();
+        teamMatesNames.add("Brayton");
+        teamMatesNames.add("Darren");
+
+        System.out.printf("Hello, my name is %s", yourName + "!\n");
+
+        for (String name : teamMatesNames             ) {
+            System.out.printf("Hello, " + yourName + " my name is %s!%n", name);
+        }
+
     }
 }
